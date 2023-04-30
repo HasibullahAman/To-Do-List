346f8b57ded6848f9da2030a1969b43afd2a272a
Hasibullah Aman <hasib.aman7@gmail.com>
2023-04-30 14:57:45 +0430

    add work section

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 
 let app = express();
 var items = [];
+var WorkItem = [];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -19,17 +20,32 @@ app.get('/', (req, res) => {
     };
     var day = today.toLocaleString('en-US', options);
     res.render('index', {
-        kindOfDay: day,
+        ListTitle: day,
         NewItems: items,
     });
 });
-
 app.post('/', (req, res) => {
-    item = req.body.newItem;
+    let item = req.body.newItem;
     items.push(item);
 
     res.redirect('/')
 });
+
+app.get('/work', (req, res) => {
+    res.render('index', {
+        ListTitle: "Work List",
+        NewItems: WorkItem,
+    })
+});
+
+app.post('/work', (req, res) => {
+    let item = req.body.newItem;
+    WorkItem.push(item);
+    res.redirect('/work');
+})
+
+
+
 
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
